95b0fb3d5a170ed360d811cd9633ce2887362f94
Filipe Lukebana <31851972+LUK3D@users.noreply.github.com>
2021-10-27 20:08:01 +0100

    Added mysql module

--- src/components/l-button.vue
+++ src/components/l-button.vue
@@ -1,6 +1,6 @@
 <template>
-  <button class="bg-blue-400 py-3 rounded-lg text-white shadow-lg transition-colors hover:bg-blue-500">
-            Teste Connexion
+  <button class="bg-blue-400 px-3 py-3 rounded-lg text-white shadow-lg transition-colors hover:bg-blue-500">
+            Teste Connection
           </button>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,35 +2,43 @@
   <div class="w-full h-full bg-gray-100 flex flex-col">
     <!-- Header section -->
     <div class="header h-16 bg-white shadow-md flex">
-      <div class="flex h-full w-20 justify-center items-center bg-blue-400 text-white font-bold text-2xl z-">
+      <div class="flex h-full w-20 justify-center items-center bg-blue-400 text-white font-bold text-2xl ">
         lF3D
       </div>
     </div>
     <!-- End header section -->
     <!-- body section -->
-    <div class="body w-full h-full p-44 flex flex-col justify-center items-center">
-      <div class="flex flex-col w-2/6 ">
+    <div class="body w-full p-0 h-full  lg:p-44 flex flex-col justify-center items-center">
+      <div class="flex flex-col lg:w-4/6 w-6/6 ">
         <p class="text-gray-400 text-xl mb-4">Connect to the database to start</p>
         <div class="loginForm w-full p-10 bg-white rounded-xl shadow-lg">
           <p class="text-2xl text-blue-400">Server Information</p>
-          <div class="grid grid-cols-1 gap-5 my-10">
-            <l-form  :model="db.host" placeholder="Ex: https://www.luk3d.com">
+          <div class="grid grid-cols-2 gap-5 my-10">
+          
+          <l-form  :model="db.host" placeholder="Ex: https://www.luk3d.com">
             <template v-slot:label> Host </template>
           </l-form>
           
-          <l-form class="mt-5"  :model="db.user" placeholder="Ex: root">
+          <l-form class="mt-2"  :model="db.user" placeholder="Ex: root">
             <template v-slot:label> Username </template>
           </l-form>
-          <l-form class="mt-5"  :model="db.pwd" placeholder="">
+          <l-form class="mt-2"  :model="db.pwd" placeholder="">
             <template v-slot:label> Password </template>
           </l-form>
+          
+          <div class="flex flex-col"> 
+            <l-form position="top" :options="options" type="select" class="mt-2"  :model="db.db" placeholder="Ex: LUK3D">
+              <template  v-slot:label> Database </template>
+            </l-form>
+          </div>
 
-          <l-button @click="testConnection(db)"></l-button>
 
-          <p class="text-md m-0 p-0 text-gray-400">Select a Database</p>
-          <l-form position="top" :options="options" type="select" class="-mt-4"  :model="db.db" placeholder="Ex: LUK3D">
-            <template  v-slot:label> Database </template>
-          </l-form>
+          <div class="col-span-2 flex ">
+            <l-button class="mr-2" @click="testConnection(db)"></l-button>
+            <l-button class="mx-2" @click="testConnection(db)"></l-button>
+          </div>
+
+          
           
           </div>
         </div>
